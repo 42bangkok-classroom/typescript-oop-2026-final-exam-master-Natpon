cfa193c01847770d108dde3529483c25b4044b28
event <event@z2t4c2.42bangkok.com>
2026-03-25 12:11:52 +0700

    jj

--- src/products/product.controller.ts
+++ src/products/product.controller.ts
@@ -3,16 +3,20 @@ import { Controller, Get, Post, Body } from '@nestjs/common';
 
 import { ProductService } from './product.service';
 import { Product } from './dto/productInterface';
-
+import { ApiResponse } from 'src/interfaces/response.interface';
 
 @Controller('Product')
 export class ProductController {
-  constructor(private ProductService: ProductService) {}
+  constructor(private ProductService: ProductService) { }
 
-  
+
 
   @Get()
-  async findAll(): Promise<Product[]>  {
-    return this.ProductService.findAll();
+  async findAll(): ApiResponse<Promise<Product[]>> {
+    return {
+      success: true,
+      data: this.ProductService.findAll(),
+      message: "Fetched products successfully"
+    }
   }
 }
